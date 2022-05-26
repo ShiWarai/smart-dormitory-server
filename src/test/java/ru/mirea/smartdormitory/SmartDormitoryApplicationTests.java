21652d900879fbc7a7556cb0a751cf4780e9a1f6
Pavel <zhuravlev.p.r@edu.mirea.ru>
2022-05-26 23:22:10 +0300

    Feat: add first test

---
+++ src/test/java/ru/mirea/smartdormitory/SmartDormitoryApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mirea.smartdormitory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartDormitoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
